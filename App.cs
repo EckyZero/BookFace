5672ce3356eaeaef3a38a5fb6d208dbf00b9453a
Daniel Esponda <daniel103@gmail.com>
2014-08-27 13:08:40 -0500

    Added dll

--- App.cs
+++ App.cs
@@ -20,7 +20,7 @@ namespace BookFace
 		{
 		}
 
-		public override void OnCreate ()
+		public async override void OnCreate ()
 		{
 			base.OnCreate ();
 			Console.WriteLine ("PARSE WAS INITIATED!!!!!");
@@ -28,6 +28,20 @@ namespace BookFace
 			// your Parse dashboard
 			ParseClient.Initialize("TlBHwmKpW4Un35ahy9vWpO8Th01ngQZ5Rg0wEqvc",
 				"Ql8f77K7n5TryGtVxAyMYrHnItqYAAujzqnY9XQ7");
+		
+
+
+			var user = new ParseUser()
+			{
+				Username = "Daniel103",
+				Password = "1234",
+				Email = "daniel103@gmail.com"
+			};
+
+			// other fields can be set just like with ParseObject
+			user["phone"] = "415-392-0202";
+
+			await user.SignUpAsync();
 		}
 	}
 }
